bedb860a6c7d1566af72487b1ee7dc616ee5b635
Olubummo Faith <aaeon456engineer@gmail.com>
2022-12-08 12:29:01 +0100

    update: set up separate docker-compose prod/dev files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World!!!!</h1>");
+    res.send("<h1>Hello World!!!!!</h1>");
 });
 
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`)) 
